7e42efcbc78ef81787b6e9f1b028048351bee58a
Ryan Linton <ryanlntn@gmail.com>
2023-07-12 04:15:57 -0600

    Add app group support for iOS (#8)
    * initial app group support
    
    * configure app group id via Info.plist AppGroup key
    
    * add appGroupID validity check

--- ios/QuickSQLite.mm
+++ ios/QuickSQLite.mm
@@ -31,11 +31,29 @@ RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(install) {
   auto &runtime = *jsiRuntime;
   auto callInvoker = bridge.jsCallInvoker;
 
-  // Get iOS app's document directory (to safely store database .sqlite3 file)
-  NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, true);
-  NSString *documentPath = [paths objectAtIndex:0];
+  // Get appGroupID value from Info.plist using key "AppGroup"
+  NSString *appGroupID = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"AppGroup"];
+  NSString *documentPath; 
 
-  osp::install(runtime, callInvoker,[documentPath UTF8String]);
+  if (appGroupID != nil) {
+    // Get the app groups container storage url
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSURL *storeUrl = [fileManager containerURLForSecurityApplicationGroupIdentifier:appGroupID];
+
+    if (storeUrl == nil) {
+      NSLog(@"Invalid AppGroup ID provided (%@). Check the value of \"AppGroup\" in your Info.plist file", appGroupID);
+      return @false;
+    }
+    NSLog(@"Configured with AppGroup ID: %@", appGroupID);
+
+    documentPath = [storeUrl path];
+  } else {
+    // Get iOS app's document directory (to safely store database .sqlite3 file)
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, true);
+    documentPath = [paths objectAtIndex:0];
+  }
+
+  osp::install(runtime, callInvoker, [documentPath UTF8String]);
   return @true;
 }
 
