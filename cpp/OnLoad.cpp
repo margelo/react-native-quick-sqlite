638fb8e8de8cc45628bac83344215a4a431f3b95
Christoph Pader <chris@margelo.io>
2024-09-05 14:54:02 +0200

    remove unnecessary JSI files

--- cpp/OnLoad.cpp
+++ cpp/OnLoad.cpp
@@ -1,19 +1,51 @@
 #include <NitroModules/HybridObjectRegistry.hpp>
+#include "HybridQuickSQLiteSpec.hpp"
+#include "HybridSelectQueryResultSpec.hpp"
+#include "sqliteBridge.h"
+
+using namespace margelo::nitro;
+
+void clearState() {
+  sqliteCloseAll();
+}
+
 
 // Call this at app startup to register the HybridObjects
-void load() {
+void registerHybridObjectConstructors() {
   HybridObjectRegistry::registerHybridObjectConstructor("QuickSQLite", []() -> std::shared_ptr<HybridObject> {
       return std::make_shared<HybridQuickSQLiteSpec>();
     });
-  HybridObjectRegistry::registerHybridObjectConstructor("QueryResult", []() -> std::shared_ptr<HybridObject> {
-      return std::make_shared<HybridQueryResultSpec>();
+  HybridObjectRegistry::registerHybridObjectConstructor("SelectQueryResult", []() -> std::shared_ptr<HybridObject> {
+      return std::make_shared<HybridSelectQueryResultSpec>();
     });
 }
 
 class OnLoad {
 public:
     OnLoad() {
-        load();
+        registerHybridObjectConstructors();
+
+        // Get appGroupID value from Info.plist using key "AppGroup"
+        NSString *appGroupID = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"ReactNativeQuickSQLite_AppGroup"];
+        NSString *documentPath;
+
+        if (appGroupID != nil) {
+          // Get the app groups container storage url
+          NSFileManager *fileManager = [NSFileManager defaultManager];
+          NSURL *storeUrl = [fileManager containerURLForSecurityApplicationGroupIdentifier:appGroupID];
+
+          if (storeUrl == nil) {
+            NSLog(@"Invalid AppGroup ID provided (%@). Check the value of \"AppGroup\" in your Info.plist file", appGroupID);
+            return @false;
+          }
+          NSLog(@"Configured with AppGroup ID: %@", appGroupID);
+
+          documentPath = [storeUrl path];
+        } else {
+          // Get iOS app's document directory (to safely store database .sqlite3 file)
+          NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, true);
+          documentPath = [paths objectAtIndex:0];
+        }
     }
 };
 
