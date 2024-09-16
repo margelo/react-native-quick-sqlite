c99cf5fbea74a16bc069507b93573c07b2fa68c8
Christoph Pader <chris@margelo.com>
2024-09-16 22:26:04 +0200

    fix: pragma once

--- package/cpp/QuickSQLiteException.hpp
+++ package/cpp/QuickSQLiteException.hpp
@@ -1,4 +1,4 @@
-#pragma
+#pragma once
 
 #include <string>
 #include <iostream>
@@ -35,27 +35,27 @@ public:
   QuickSQLiteException(const QuickSQLiteExceptionType type, const char* message) : QuickSQLiteException(type, std::string(message)) {}
   QuickSQLiteException(const QuickSQLiteExceptionType type, std::string message)
     : _exceptionString("[react-native-quick-sqlite] " + typeToString(type) + ": " + message) {}
-  
+
 private:
   const std::string _exceptionString;
-  
+
 public:
   const char* what() const noexcept override {
     return this->_exceptionString.c_str();
   }
-  
+
   static QuickSQLiteException DatabaseNotOpen(std::string dbName) {
     return QuickSQLiteException(QuickSQLiteExceptionType::UnableToAttachToDatabase, dbName + " is not open");
   }
-  
+
   static QuickSQLiteException SqlExecution(std::string errorMessage) {
     return QuickSQLiteException(QuickSQLiteExceptionType::SqlExecutionError, errorMessage);
   }
-  
+
   static QuickSQLiteException DatabaseFileNotFound(std::string dbFilePath) {
     return QuickSQLiteException(QuickSQLiteExceptionType::SqlExecutionError, "Database file not found: " + dbFilePath);
   }
-  
+
   static QuickSQLiteException CouldNotLoadFile(std::string fileLocation, std::optional<std::string> additionalLine = std::nullopt) {
     const auto message = "Could not load file: " + fileLocation + (additionalLine ? (". " + *additionalLine) : "");
     return QuickSQLiteException(QuickSQLiteExceptionType::CouldNotLoadFile, message);
