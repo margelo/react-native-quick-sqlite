8f7d88602fbcb8589b3b5d03eb7f1862d321c72a
Christoph Pader <chris@margelo.com>
2024-09-17 19:00:25 +0200

    android context catch

--- package/android/src/main/cpp/cpp-adapter.cpp
+++ package/android/src/main/cpp/cpp-adapter.cpp
@@ -7,17 +7,15 @@
 
 using namespace margelo::nitro::rnquicksqlite;
 
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_margelo_rnquicksqlite_RNQuickSQLiteModule_setDocPath__Ljava_lang_String_2(JNIEnv *env,
-                                                                                   jobject thiz,
-                                                                                   jstring doc_path) {
-    const char *nativeString = env->GetStringUTFChars(doc_path, nullptr);
-    HybridQuickSQLite::docPath = std::string(nativeString);
-    env->ReleaseStringUTFChars(doc_path, nativeString);
-}
-
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
     return margelo::nitro::rnquicksqlite::initialize(vm);
 }
 
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_margelo_rnquicksqlite_RNQuickSQLitePackage_setDocPathInJNI__Ljava_lang_String_2(
+    JNIEnv *env, jclass clazz, jstring doc_path) {
+  const char *nativeString = env->GetStringUTFChars(doc_path, nullptr);
+  HybridQuickSQLite::docPath = std::string(nativeString);
+  env->ReleaseStringUTFChars(doc_path, nativeString);
+}
--- package/android/src/main/java/com/margelo/rnquicksqlite/RNQuickSQLitePackage.java
+++ package/android/src/main/java/com/margelo/rnquicksqlite/RNQuickSQLitePackage.java
@@ -1,25 +1,36 @@
 package com.margelo.rnquicksqlite;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
 import com.facebook.react.TurboReactPackage;
+import com.margelo.nitro.NitroModulesPackage;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 public class RNQuickSQLitePackage extends TurboReactPackage {
   static {
     System.loadLibrary("RNQuickSQLite");
   }
 
-  @Nullable
-  @Override
-  public NativeModule getModule(String name, ReactApplicationContext reactContext) {
-    return null;
+  private native static void setDocPathInJNI(String docPath);
+
+  public static void setDocPath(ReactApplicationContext context) {
+    final String path =  context.getFilesDir().getAbsolutePath();
+    setDocPathInJNI(path);
   }
 
+  @Nullable
+  @Override
+  public NativeModule getModule(String name, ReactApplicationContext reactContext) {return null;}
+
   public RNQuickSQLitePackage() {
+    ReactApplicationContext context = NitroModulesPackage.getReactContext();
+    setDocPath(context);
   }
 
   @Override
